f436a0ef9e5291ccdf3975da4574a352af357df2
maria <maria.gabrielly7825@gmail.com>
2024-09-30 12:37:09 -0300

    exercicio while

---
+++ exercicioJava/src/lacosderepeticao/Aula01Introducao.java
@@ -0,0 +1,11 @@
+package lacosderepeticao;
+
+public class Aula01Introducao {
+	public static void main(String[] args) {
+		int contador = 0;
+		while (contador <= 10) {
+			System.out.println("contador: " + contador);
+			contador = contador + 1;
+	}
+	}
+}
