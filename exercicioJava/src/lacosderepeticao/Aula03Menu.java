99ebeb637c70f1b98ba6a07426d3cdb6d7421d55
maria <maria.gabrielly7825@gmail.com>
2024-10-03 15:02:57 -0300

    laço de repetição while

---
+++ exercicioJava/src/lacosderepeticao/Aula03Menu.java
@@ -0,0 +1,19 @@
+package lacosderepeticao;
+
+import java.util.Scanner;
+
+public class Aula03Menu {
+	public static void main(String[] args) {
+		int opcao = 0;
+		Scanner teclado = new Scanner(System.in);
+		while(opcao != 3) {
+			System.out.println("1. Calcular imposto");
+			System.out.println("2. Depositar Salário");
+			System.out.println("3. Sair");
+			System.out.println("Digite a sua opção");
+			opcao = teclado.nextInt();
+		}
+		System.out.println("Fim do programa");
+	}
+
+}
